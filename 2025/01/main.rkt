528a682dc0c1d4a23565fbaedba1cadf66d6b824
Christopher Fujino <christopherfujino@gmail.com>
2025-12-08 12:30:26 -0800

    clean; gitignore *.rkt~

--- 2025/01/main.rkt
+++ 2025/01/main.rkt
@@ -2,26 +2,25 @@
 
 #lang racket
 
-(let ([lines
-       ((compose string-split port->string open-input-file) "input.txt")])
-  (let ([pos_zeroes
-         (foldl
-          (lambda (line pos_zeroes_pair)
-            (let ([pos (car pos_zeroes_pair)]
+(let* ([lines
+        ((compose string-split port->string open-input-file) "input.txt")]
+       [pos_zeroes
+        (foldl
+         (lambda (line pos_zeroes_pair)
+           (let* ([pos (car pos_zeroes_pair)]
                   [zeroes (cdr pos_zeroes_pair)]
-                  [cmd (substring line 0 1) ])
-              (let ([distance (string->number (substring line 1))])
-                (let ([pos (match cmd
-                             ["L" (- pos distance)]
-                             ["R" (+ pos distance)]
-                             [_ (error "Crash!")])])
-                  (let ([pos (modulo pos 100)])
-                    (let ([zeroes (if (= pos 0)
-                                      (+ zeroes 1)
-                                      zeroes)])
-                      (cons pos zeroes)))))))
-            ; initializer
-            (cons 50 0)
-            lines)])
-        (printf "Number of zeroes: ~a\n" (cdr pos_zeroes))))
-  
+                  [cmd (substring line 0 1) ]
+                  [distance (string->number (substring line 1))]
+                  [pos (match cmd
+                         ["L" (- pos distance)]
+                         ["R" (+ pos distance)]
+                         [_ (error "Crash!")])]
+                  [pos (modulo pos 100)]
+                  [zeroes (if (= pos 0)
+                              (+ zeroes 1)
+                              zeroes)])
+             (cons pos zeroes)))
+         ; initializer
+         (cons 50 0)
+         lines)])
+  (printf "Number of zeroes: ~a\n" (cdr pos_zeroes)))
